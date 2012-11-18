ffcb97206706ac7262aa991200b9967119650494
cosmicdreams <christopher.m.weber@gmail.com>
2012-11-17 20:31:46 -0600

    fix proper calls to panel region variables.

--- sites/all/themes/jjs/panels/jj_business/jj-business.tpl.php
+++ sites/all/themes/jjs/panels/jj_business/jj-business.tpl.php
@@ -1,25 +1,25 @@
 <?php if ($header): ?>
 <div class="jj-header">
-  <?php print $header; ?>
+  <?php print $content['header']; ?>
 </div>
 <?php endif; ?>
 
 <div class="jj-wrap">
-  <?php if ($left): ?>
+  <?php if ($content['left']): ?>
   <div class="jj-left">
-    <?php print $left; ?>
+    <?php print $content['left']; ?>
   </div>
   <?php endif; ?>
 
-  <?php if ($right): ?>
+  <?php if ($content['right']): ?>
   <div class="jj-right">
-    <?php print $right; ?>
+    <?php print $content['right']; ?>
   </div>
   <?php endif; ?>
 </div>
 
-<?php if ($footer): ?>
+<?php if ($content['footer']): ?>
 <div class="jj-footer">
-  <?php print $footer; ?>
+  <?php print $content['footer']; ?>
 </div>
 <?php endif; ?>
