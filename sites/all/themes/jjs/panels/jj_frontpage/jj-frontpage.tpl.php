bce3d2396bd96cc803c9c47cfeae5468b36b6ec7
Dillon <burlapdesign@dillonross.info>
2013-01-17 17:37:52 -0600

    Modifying panels for use with Omega.

--- sites/all/themes/jjs/panels/jj_frontpage/jj-frontpage.tpl.php
+++ sites/all/themes/jjs/panels/jj_frontpage/jj-frontpage.tpl.php
@@ -1,15 +1,15 @@
-<div class="jj-wrap">
-	<div class="jj-header-left">
+<div class="jj-wrap container-12">
+	<!-- <div class="jj-header-left">
 	  <?php print $content['header_left']; ?>
 	</div>
 	<div class="jj-header-right">
 	  <?php print $content['header_right']; ?>
-	</div>
+	</div>  using panels only for the content portion-->
 
-	<div class="jj-blog">
+	<div class="jj-blog grid-4">
 	  <?php print $content['blog']; ?>
 	</div>
-	<div class="jj-reviews">
+	<div class="jj-reviews grid-8">
 	  <?php print $content['reviews']; ?>
 	</div>
 </div>
